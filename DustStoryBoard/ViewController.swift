22d5855ffd7b1ac0fd0c511de8ca776ff92b4bab
Eum <dheum@duksung.ac.kr>
2020-07-11 11:58:57 +0900

    Initial Commit

---
+++ DustStoryBoard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DustStoryBoard
+//
+//  Created by dheum on 2020/07/11.
+//  Copyright © 2020 dheum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
